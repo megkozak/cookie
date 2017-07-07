47fa90e5b422c10a9731afd832fe9638dbd25331
Meg Kozak <megkozak@gmail.com>
2017-07-07 14:49:28 -0400

    got cookie counter working separately

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,29 +3,42 @@ var chocolate = document.querySelector('.choco')
 var lemon = document.querySelector('.lemon')
 var oatmeal = document.querySelector('.oat')
 // when the page loads, grab the value of the saved cookie, by its name
-var clicks = Cookies.get('chocolate')
+var chocolateCookie = Cookies.get('chocolate') || 0
+var lemonCookie = Cookies.get('lemon') || 0
+var oatmealCookie = Cookies.get('oatmeal') || 0
 
+document.querySelector('#choco').innerText = chocolateCookie
 
-chocolate.addEventListener('click', function() {
+chocolate.addEventListener('click', function(e) {
+  e.preventDefault()
+  console.log(chocolateCookie)
+
   //add one to click
-  clicks++
+  chocolateCookie++
   //set the new value
-  Cookies.set('chocolate', clicks)
-  // document.getElementById("chocolate").innerHTML = clicks
+  Cookies.set('chocolate', chocolateCookie)
+  // console.log('fdsaf', chocolateCookie);
+  document.querySelector('#choco').innerText = chocolateCookie
 
   console.log(Cookies.get('chocolate'))
 })
 
-lemon.addEventListener('click', function() {
-   clicks++
-   Cookies.set('lemon', clicks)
+lemon.addEventListener('click', function(e) {
+  e.preventDefault()
+
+   lemonCookie++
+   Cookies.set('lemon', lemonCookie)
+   document.querySelector('#lemon').innerText = lemonCookie
 
    console.log(Cookies.get('lemon'))
  })
 
- oatmeal.addEventListener('click', function() {
-    clicks++
-    Cookies.set('oatmeal', clicks)
+ oatmeal.addEventListener('click', function(e) {
+   e.preventDefault()
+
+    oatmealCookie++
+    Cookies.set('oatmeal', oatmealCookie)
+    document.querySelector('#oat').innerText = oatmealCookie
 
     console.log(Cookies.get('oatmeal'))
   })
