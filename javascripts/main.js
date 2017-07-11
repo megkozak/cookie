88629f9e10bbb7811c2a1901cd742544e1bb09dc
Meg Kozak <megkozak@gmail.com>
2017-07-11 19:02:59 -0400

    changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,43 +2,57 @@
 var chocolate = document.querySelector('.choco')
 var lemon = document.querySelector('.lemon')
 var oatmeal = document.querySelector('.oat')
-// when the page loads, grab the value of the saved cookie, by its name
+// var total= document.querySelector('.sum')
+
+
 var chocolateCookie = Cookies.get('chocolate') || 0
 var lemonCookie = Cookies.get('lemon') || 0
 var oatmealCookie = Cookies.get('oatmeal') || 0
+var totalCookie = ('chocolate' + 'lemon' + 'oatmeal')
 
-document.querySelector('#choco').innerText = chocolateCookie
 
+
+// document.querySelector('#choco').innerText = chocolateCookie
+    // console.log(chocolateCookie)
 chocolate.addEventListener('click', function(e) {
   e.preventDefault()
   console.log(chocolateCookie)
-
-  //add one to click
-  chocolateCookie++
-  //set the new value
+    chocolateCookie++
   Cookies.set('chocolate', chocolateCookie)
-  // console.log('fdsaf', chocolateCookie);
   document.querySelector('#choco').innerText = chocolateCookie
-
   console.log(Cookies.get('chocolate'))
 })
 
 lemon.addEventListener('click', function(e) {
   e.preventDefault()
+    lemonCookie++
+  Cookies.set('lemon', lemonCookie)
+  document.querySelector('#lemon').innerText = lemonCookie
+  console.log(Cookies.get('lemon'))
+})
 
-   lemonCookie++
-   Cookies.set('lemon', lemonCookie)
-   document.querySelector('#lemon').innerText = lemonCookie
-
-   console.log(Cookies.get('lemon'))
- })
-
- oatmeal.addEventListener('click', function(e) {
-   e.preventDefault()
-
+oatmeal.addEventListener('click', function(e) {
+  e.preventDefault()
     oatmealCookie++
-    Cookies.set('oatmeal', oatmealCookie)
-    document.querySelector('#oat').innerText = oatmealCookie
+  Cookies.set('oatmeal', oatmealCookie)
+  document.querySelector('#oat').innerText = oatmealCookie
+  console.log(Cookies.get('oatmeal'))
+})
 
-    console.log(Cookies.get('oatmeal'))
-  })
+
+
+
+function clearcookies() {
+  	Cookies.set("chocolate", 0);
+  	Cookies.set("lemon", 0);
+  	Cookies.set("oatmeal", 0);
+  	Cookies.set("total", 0);
+  	// displaycookie_count();
+  }
+
+
+
+
+
+
+  
